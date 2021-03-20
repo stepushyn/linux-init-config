5332452f2e593f1d1c7dc55399c1af39abd4040b
Aleks Step <aleks@stepushyn1.example.com>
2021-03-20 23:24:43 +0300

    init

---
+++ script.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Create a new user
+echo ""
+read -p "Enter username: " username
+sudo adduser "$username" -q --gecos ""
+echo -e "\nNew user created \n"
+
+
+
+# Add a new user to the sudo group
+usermod -aG sudo $username
+echo -e "User added to sudo group \n"
+
+
+
+# Create a .ssh folder
+if [ ! -d "/home/$username/.ssh" ]
+then
+	mkdir /home/$username/.ssh
+	echo -e ".ssh folder created \n"
+fi
+
+
+
+# Writing the public key to authorized_keys
+read -p "Enter pub key: " pubkey
+echo "$pubkey" > /home/$username/.ssh/authorized_keys
+echo -e "\nPub key saved to authorized_keys \n"
+
+
+
+# Change access rights
+chmod -R go= /home/$username/.ssh/
+chown -R $username:$username /home/$username/.ssh/
+echo -e "Access rights changed \n"
+
