877968c2cd0d447e3efa9d6b9f934c0fd7ced8e5
Aleks Step <aleks@stepushyn1.example.com>
2021-03-21 13:24:05 +0300

    ufw changes

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 # Create a new user
-echo ""
 read -p "Enter username: " username
 sudo adduser "$username" -q --gecos ""
-echo "\nNew user created"
+echo "New user created"
 
 
 
@@ -48,33 +47,37 @@ then
 	mkdir /etc/ssh/sshd_config.d
 fi
 
+
+
 read -p "Ban root login (y/n) " rootlogin
 if [[ $rootlogin = "y" || $rootnogin = "Y" ]]
 then
-	echo -e "PermitRootLogin no\n" > /etc/ssh/sshd_config.d/my-config.conf
+	echo "PermitRootLogin no" > /etc/ssh/sshd_config.d/my-config.conf
 	echo "Root login banned"
 fi
 
 
-read -p "Ban password Authentication (y/n)" passwordauth
+
+read -p "Ban password Authentication (y/n) " passwordauth
 if [[ $passwordauth = "y" || $passwordauth = "Y" ]]
 then
-	echo -e "PasswordAuthentication no\n" >> /etc/ssh/sshd_config.d/my-config.conf
+	echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/my-config.conf
 	echo "Password Authentication banned"
 fi
 
 
-read -p "Change SSH port? (y/n) " port
-if [[ $port = "y" || $port = "Y" ]]
+
+read -p "Enter SSH port (default 22): " port
+if [ port -ne 22 ]
 then
-	read -p "Enter SSH port (default 22): " port
-	echo -e "Port $port\n" >> /etc/ssh/sshd_config.d/my-config.conf
+	echo "Port $port" >> /etc/ssh/sshd_config.d/my-config.conf
 	echo "SSH port changed to $port"
 fi
 
 
-read -p "Restart SSH daemon? (y/n) " restart
-if [[ $restart = "y" || $restart = "Y" ]]
+
+read -p "Restart SSH daemon? (y/n) " restartssh
+if [[ $restartssh = "y" || $restartssh = "Y" ]]
 then
 	systemctl restart sshd
 	echo "SSH restarted"
@@ -85,18 +88,24 @@ fi
 # UFW configuration
 ####################################
 ufw allow $port
+
+
+
 read -p "Ban IPv6? (y/n) " banipv6
 if [[ $banipv6 = "y" || $banipv6 = "Y" ]]
 then
-	cp /etc/default/ufw /etc/default/ufw.old
-	cp ufw_ipv6_no /etc/default/ufw
+	sed -i -e 's/IPV6=yes/IPV6=no/g' /etc/default/ufw
 fi
+
+
 
 read -p "Enable UFW? (y/n) " enableufw
 if [[ $enableufw = "y" || $enableufw = "Y" ]]
 then
 	ufw enable
+	echo "UFW enabled"
 fi
+
 
 
 # to do:
